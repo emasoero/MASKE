e0eda2e1734542d8b1c8740581d0d874c3a4ae03
Denis Taniguchi <denis.taniguchi@ncl.ac.uk>
2019-11-25 18:45:05 +0000

    Adding possibility to wait for gdb attachment.

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,18 @@ int main(int argc, char **argv)
 {
     
     MPI_Init(&argc,&argv);
-
     
-	// Create an object from the fundamental (this object is essentially your whole simulation)
+#ifdef MASKE_WAIT_ATTACH
+    int i = 0;
+    char hostname[256];
+    gethostname(hostname, sizeof(hostname));
+    printf("PID %d on %s ready for attach\n", getpid(), hostname);
+    fflush(stdout);
+    while (0 == i)
+      sleep(5);
+#endif
+
+    // Create an object from the fundamental (this object is essentially your whole simulation)
     MASKE *maske = new MASKE(argc,argv);
     
     
